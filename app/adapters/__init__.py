1436b233c7db1eaf6c323530df00dc1a9653fc4c
schneidermatic <markus.schneider73@gmail.com>
2020-12-24 18:55:12 +0100

    Initial commit of the Adapter class stub.
     On branch master
     Changes to be committed:
    	modified:   app/adapters/__init__.py

--- app/adapters/__init__.py
+++ app/adapters/__init__.py
@@ -1,2 +1,9 @@
  # -*- coding: utf-8 -*-
  
+ class Adapter:
+    def __init__(self, obj, adapted_methods):
+        self.obj = obj
+        self.__dict__.update(adapted_methods)
+        
+    def __str__(self):
+        return str(self.obj)
