8bfec036e660fc2d93e09284eafb7630fda9fee2
schneidermatic <markus.schneider73@gmail.com>
2020-12-29 19:36:13 +0100

    Class Adatpter was refactored.
     On branch master
     Changes to be committed:
    	modified:   app/adapters/__init__.py

--- app/adapters/__init__.py
+++ app/adapters/__init__.py
@@ -1,9 +1,15 @@
  # -*- coding: utf-8 -*-
- 
- class Adapter:
-    def __init__(self, obj, adapted_methods):
-        self.obj = obj
-        self.__dict__.update(adapted_methods)
+
+class Adapter:
+
+    def __init__(self, name, app):
+        self.name = name
+        self.app  = app
+        self.index_01 = self.app.config['APP_ES_INDEX_01']
+        self.index_02 = self.app.config['APP_ES_INDEX_02']
+
+    def execute(self, json_data):
+        pass
         
     def __str__(self):
         return str(self.obj)
