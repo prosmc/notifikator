54e5267efbeb834439849e19b9e6170ad6550763
schneidermatic <markus.schneider73@gmail.com>
2020-12-31 12:16:21 +0100

    Class 'Adapter' was removed from '__init__.py' and moved to separate file.
     On branch master
     Changes to be committed:
    	modified:   app/adapters/__init__.py

--- app/adapters/__init__.py
+++ app/adapters/__init__.py
@@ -1,16 +1,1 @@
  # -*- coding: utf-8 -*-
-
-class Adapter:
-
-    def __init__(self, app, adapter):
-        self.app      = app
-        self.adapter  = adapter
-
-    def send(self, json_data):
-        self.adapter.send(json_data)
-    
-    def request(self, json_data):
-        self.adapter.request(json_data)
-
-    def __str__(self):
-        return str(self.obj)
