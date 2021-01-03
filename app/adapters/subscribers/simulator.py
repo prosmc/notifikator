247cdf6aa48ffa82702129531f20b2571c694b32
schneidermatic <markus.schneider73@gmail.com>
2021-01-03 18:30:46 +0100

    Initial commit of the class 'SimulatorAdapter' of type subscriber.
     On branch master
     Changes to be committed:
    	new file:   app/adapters/subscribers/simulator.py

---
+++ app/adapters/subscribers/simulator.py
@@ -0,0 +1,40 @@
+ # -*- coding: utf-8 -*-
+
+from app.utils import Timer, TimeStamp
+from app.client import ClientFactory, ClientType
+from flask import current_app
+import json
+import uuid
+
+class SimulatorAdapter():
+
+    def __init__(self, app, name):
+        self.app      = app
+        self.name     = name
+        self.client   = ClientFactory.newInstance(ClientType.ELASTICSEARCH, self.app)
+        self.index_01 = self.app.config['APP_ES_INDEX_01']
+        self.index_02 = self.app.config['APP_ES_INDEX_02']
+
+    #TODO: Method must be implemented.
+    def filter(self, json_data):
+        pass
+
+    #TODO: Method must be implemented.
+    def get_search_query_result(self):
+        pass
+
+    #TODO: Method must be implemented.
+    def read(self, json_data):
+        pass
+
+    #TODO: Method must be implemented.
+    def update(self, json_data):
+        pass
+
+    #TODO: Method must be implemented.
+    def request(self, json_data):
+        with self.app.app_context():
+            pass
+
+    def __str__(self):
+        return f"Sender Adapter Name: { self.name }"
