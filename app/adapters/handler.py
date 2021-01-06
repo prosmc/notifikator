b1735333d7466c93b8230c7667ec576979957f04
schneidermatic <markus.schneider73@gmail.com>
2021-01-06 18:14:17 +0100

    Sync adapter functinality  was activated in 'handler.py'.
     On branch master
     Your branch is ahead of 'origin/master' by 13 commits.
       (use "git push" to publish your local commits)
    
     Changes to be committed:
    	modified:   app/adapters/handler.py

--- app/adapters/handler.py
+++ app/adapters/handler.py
@@ -15,12 +15,12 @@ class AdapterHandler():
     def activate(self, json_data, adapters):
         with self.app.app_context():
             for adapter in adapters:
-                current_app.logger.debug("Adapter { adapter.name } is executed.")
+                current_app.logger.debug(f"Adapter { adapter.name } is executed.")
                 adapter_instance = Adapter(self.app, adapter)
-                adapter_instance.run(json_data)
+                adapter_instance.run(json_data=json_data)
 
     def run(self, processor_type, json_data):
         if (processor_type == ProcessorType.TYPE_01.value):
             self.activate(json_data=json_data, adapters=self.pub_adapters)
-        #if (processor_type == ProcessorType.TYPE_02.value):
-        #    self.adapter_handler.run_synchronizer_adapters(json_data=doc, adapters=self.sync_adapters)
+        if (processor_type == ProcessorType.TYPE_02.value):
+            self.activate(json_data=json_data, adapters=self.sync_adapters)
